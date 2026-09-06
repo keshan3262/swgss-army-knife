98df86b8e68c0904ddb3407fa60fba8a472d9e57
Inokentii Mazhara <keshan3262@gmail.com>
2026-09-06 09:49:50 +0300

    Update env schema

--- src/config/env.schema.ts
+++ src/config/env.schema.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 export const envSchema = z.object({
   PORT: z.coerce.number().int().min(1).max(65535).default(3000),
   BASE_URL: z.url({ protocol: /^http(s)?$/ }).optional(),
-  DB_URL: z.string().regex(/^postgresql:\/\/[^:@]+:@[^:]+:\d+\/[^?]+(\?.*)?$/),
+  DB_URL: z.string().regex(/^postgresql:\/\/[^:@]+@[^:]+:\d+\/[^?]+(\?.*)?$/),
   REDIS_URL: z.url({ protocol: /^redis$/ })
 }).superRefine(
   (data) => {
